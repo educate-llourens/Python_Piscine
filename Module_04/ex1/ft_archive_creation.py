0b9c58a2ec1dddc16561dfa7ca6f4b85e3659737
llourens <llourens@f0r3s7.codam.nl>
2026-02-23 17:10:52 +0100

    🚧 Created Module_04 branch and did exercises 0 - 4

---
+++ Module_04/ex1/ft_archive_creation.py
@@ -0,0 +1,25 @@
+#!/usr/bin/env python3
+
+def main() -> None:
+    print("=== CYBER ARCHIVES - PRESERVATION SYSTEM ===\n")
+    print("Initializing new storage unit: new_discovery.txt")
+    try:
+        file = open("new_discovery.txt", "w+")
+        print("Storage unit created successfully...\n")
+        print("Inscribing preservation data...")
+        file.write("[ENTRY 001] New quantum algorithm discovered\n")
+        file.write("[ENTRY 002] Efficiency increased by 347%\n")
+        file.write("[ENTRY 003] Archived by Data Archivist trainee")
+        print("[ENTRY 001] New quantum algorithm discovered")
+        print("[ENTRY 002] Efficiency increased by 347%")
+        print("[ENTRY 003] Archived by Data Archivist trainee\n")
+        file.close()
+    except PermissionError:
+        print("Error: You do not have permission to write this file")
+        return
+    print("Data inscription complete. Storage unit sealed")
+    print("Archive 'new_discovery.txt' ready for long-term preservation.")
+
+
+if __name__ == "__main__":
+    main()
