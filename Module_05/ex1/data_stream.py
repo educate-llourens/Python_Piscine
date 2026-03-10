ef7595fcba1562deaf7bbc4ae2890e25c29cf937
llourens <llourens@f0r3s7.codam.nl>
2026-03-10 16:46:34 +0100

    🚧 Made some progress on ex1

--- Module_05/ex1/data_stream.py
+++ Module_05/ex1/data_stream.py
@@ -1,2 +1,53 @@
 #!/usr/bin/env python3
 
+from abc import ABC, abstractmethod
+from typing import Any, Optional, List, Dict, Union
+
+
+class DataStream(ABC):
+    def __init__(self, stream_id: str) -> None:
+        self.stream_id = stream_id
+
+    @abstractmethod
+    def process_batch(self, data_batch: List[Any]) -> str:
+        raise NotImplementedError("Subclass must implement process_batch")
+
+    def filter_data(self, data_batch: List[Any],
+                    criteria: Optional[str] = None) -> List[Any]:
+        pass
+
+    def get_stats(self) -> Dict[str, Union[str, int, float]]:
+        pass
+
+
+class SensorStream(DataStream):
+    def __init__(self, stream_id: str) -> None:
+        super().__init__(stream_id)
+
+    def process_batch(self, data_batch: List[Any]) -> str:
+        pass
+
+
+class TransactionStream(DataStream):
+    def __init__(self, stream_id: str) -> None:
+        super().__init__(stream_id)
+
+    def process_batch(self, data_batch: List[Any]) -> str:
+        pass
+
+
+class EventStream(DataStream):
+    def __init__(self, stream_id: str) -> None:
+        super().__init__(stream_id)
+
+    def process_batch(self, data_batch: List[Any]) -> str:
+        pass
+
+
+def main() -> None:
+    print("=== CODE NEXUS - POLYMORPHIC STREAM SYSTEM ===")
+    print("")
+
+
+if __name__ == "__main__":
+    main()
