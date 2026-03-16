99b36da1d1ae5f9cd92a92c8000d58dc9a97a3f1
Leandra Lourens <llourens.codam@gmail.com>
2026-03-16 15:34:14 +0100

    🚧 TransactionStream: - Filter data now filters for buy and sell, turns sell items into negative numbers - process batch calculates the net flow

--- Module_05/ex1/data_stream.py
+++ Module_05/ex1/data_stream.py
@@ -48,22 +48,30 @@ class SensorStream(DataStream):
                     criteria: Optional[str] = None) -> List[Any]:
         temp_readings: List[float] = []
         criteria_data: List[float] = []
+        new_data_batch: List[Any] = []
 
-        for item in data_batch:
+        if criteria:
+            criteria_data = [item for item in data_batch if
+                             item != criteria]
+            new_data_batch = criteria_data
+        else:
+            new_data_batch = data_batch
+        for item in new_data_batch:
             if not isinstance(item, str):
                 continue
             key, value = item.split(":", 1)
             if key == "temp":
-                temp_readings.append(float(value))
+                try:
+                    temp_readings.append(float(value))
+                except ValueError:
+                    raise ValueError(f"Error: ValueError. Sensor stream ID: "
+                                     f"{self.stream_id} cannot convert"
+                                     f"{value} to a float")
             else:
                 continue
             if not temp_readings:
                 raise RuntimeError(f"Error: Sensor stream ID {self.stream_id} "
                                    "has no temperature readings")
-            if criteria:
-                criteria_data = [item for item in data_batch if
-                                 item != criteria]
-                return criteria_data
         return temp_readings
 
     def get_stats(self) -> Dict[str, Union[str, int, float]]:
@@ -78,17 +86,53 @@ class TransactionStream(DataStream):
         super().__init__(stream_id)
 
     def process_batch(self, data_batch: List[Any]) -> str:
+        net_flow: int = 0
+        return_str: str = ""
+        operations_len: int = len(data_batch)
+
         if not data_batch:
-            raise RuntimeError(f"Error: Transaction stream ID {self.stream_id} has "
-                               "no transactions")
-        return ""
+            raise RuntimeError(f"Error: Transaction stream ID {self.stream_id}"
+                               " has no transactions")
+        net_flow = sum(data_batch)
+        if net_flow > 0:
+            return_str = (f"{operations_len} operations, net flow "
+                          f"+{net_flow} units")
+        elif net_flow <= 0:
+            return_str = (f"{operations_len} operations, net flow "
+                          f"{net_flow} units")
+        return return_str
 
     def filter_data(self, data_batch: List[Any],
                     criteria: Optional[str] = None) -> List[Any]:
-        if not criteria:
-            return data_batch
+        transactions_list: List[int] = []
+        criteria_data: List[int] = []
+        new_transaction_batch: List[Any] = []
+        negative: int = 0
+
+        if criteria:
+            criteria_data = [item for item in data_batch if item != criteria]
+            new_transaction_batch = criteria_data
         else:
-            data_batch = [item for item in data_batch if item != criteria]
+            new_transaction_batch = data_batch
+        for item in new_transaction_batch:
+            if not isinstance(item, str):
+                continue
+            key, value = item.split(":", 1)
+            if key == "buy":
+                try:
+                    transactions_list.append(int(value))
+                except ValueError:
+                    raise ValueError(f"Error: ValueError. Transaction stream "
+                                     f"ID: {self.stream_id} cannot convert "
+                                     f"{value} to an int")
+            elif key == "sell":
+                try:
+                    negative = int(value)
+                    transactions_list.append(negative * -1)
+                except ValueError:
+                    raise ValueError("Error: ValueError. Transaction stream "
+                                     f"ID: {self.stream_id} cannot convert "
+                                     f"{value} to an int")
         return data_batch
 
 
@@ -113,8 +157,11 @@ class EventStream(DataStream):
 
 def main() -> None:
     sensor: SensorStream = SensorStream("SENSOR_001")
-    sensor_batch: list[Any] = ["temp:22.5", "humidity:65", "pressure:1013"]
+    sensor_batch: List[Any] = ["temp:22.5", "humidity:65", "pressure:1013"]
     sensor_filtered_data: List[float] = []
+    transaction: TransactionStream = TransactionStream("TRANS_001")
+    transactions_batch: List[Any] = ["buy:100", "sell:150", "buy:75"]
+    transaction_filtered_data: list[int] = []
 
     print("=== CODE NEXUS - POLYMORPHIC STREAM SYSTEM ===")
     print("")
@@ -124,11 +171,20 @@ def main() -> None:
     try:
         sensor_filtered_data = sensor.filter_data(sensor_batch)
         print(f"Sensor analysis: {sensor.process_batch(sensor_filtered_data)}")
-    except (RuntimeError, TypeError) as msg:
+    except (RuntimeError, TypeError, ValueError) as msg:
         print(msg)
         return
     print("")
     print("Initializing Transaction Stream...")
+    print(f"Stream ID: {transaction.stream_id}, Type: Financial Data")
+    print("Processing transaction batch: [buy:100, sell:150, buy:75]")
+    try:
+        transaction_filtered_data = transaction.filter_data(transactions_batch)
+        print("Transaction analysis: "
+              f"{transaction.process_batch(transaction_filtered_data)}")
+    except (RuntimeError, ValueError) as msg:
+        print(msg)
+        return
 
 
 if __name__ == "__main__":
