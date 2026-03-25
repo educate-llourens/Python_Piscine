4ae03997d1de2b745f268e187bce22fa6ee0f082
Leandra Lourens <llourens.codam@gmail.com>
2026-03-25 20:47:56 +0100

    🚧 Progressed on ex2

--- Module_05/ex2/nexus_pipeline.py
+++ Module_05/ex2/nexus_pipeline.py
@@ -2,13 +2,7 @@
 
 from abc import ABC, abstractmethod
 from typing import Protocol, Any, List, Union, Dict
-
-
-class NexusManager:
-    def __init__(self) -> None:
-        pipelines: List[ProcessingPipeline]
-    
-    def add_pipeline() -> None:
+from collections import defaultdict
 
 
 class ProcessingStage(Protocol):
@@ -19,18 +13,17 @@ class ProcessingStage(Protocol):
     Args:
         Protocol (_type_): _description_
     """
-    def process(self, data: Any) -> Any:...
-        pass
+    def process(self, data: Any) -> Any: ...
 
 
-# ABC -------------------------------------------------------------------------
+# CREATING THE PIPELINE *******************************************************
 class ProcessingPipeline(ABC):
     """
     Abstract base managing stages. contains a list of stages and orchestrates
     data flow
     """
     def __init__(self):
-        self.stages = []
+        self.stages: List[ProcessingStage] = []
 
     def add_stage(self, stage: ProcessingStage) -> None:
         self.stages.append(stage)
@@ -40,7 +33,7 @@ class ProcessingPipeline(ABC):
         pass
 
 
-# STAGE CLASSES ---------------------------------------------------------------
+# STAGE CLASSES ***************************************************************
 class InputStage:
     def process(self, data: Any) -> Dict:
         return_dict: dict = {}
@@ -59,11 +52,14 @@ class OutputStage:
         return return_str
 
 
-# ADAPTER CLASSES -------------------------------------------------------------
+# ADAPTER CLASSES *************************************************************
 class JSONAdapterPipeLine(ProcessingPipeline):
     def __init__(self, pipeline_id: str):
         super().__init__()
         self.pipeline_id = pipeline_id
+
+    def process(self, data: Any) -> Any:
+        pass
 
 
 class CSVAdapterPipeLine(ProcessingPipeline):
@@ -71,33 +67,74 @@ class CSVAdapterPipeLine(ProcessingPipeline):
         super().__init__()
         self.pipeline_id = pipeline_id
 
+    def process(self, data: Any) -> Any:
+        pass
+
 
 class StreamAdapterPipeLine(ProcessingPipeline):
     def __init__(self, pipeline_id: str):
         super().__init__()
         self.pipeline_id = pipeline_id
 
+    def process(self, data: Any) -> Any:
+        pass
 
-# PRINT EXAMPLE ---------------------------------------------------------------
+
+# NEXUS MANAGER ***************************************************************
+class NexusManager:
+    def __init__(self) -> None:
+        self.pipelines: List[ProcessingPipeline] = []
+
+    def add_pipeline(self, pipeline: ProcessingPipeline) -> None:
+        self.pipelines.append(pipeline)
+
+
+# PRINT EXAMPLE ***************************************************************
 def main() -> None:
-    print("=== CODE NEXUS - ENTERPRISE PIPELINE SYSTEM ===")
-    print("")
-    print("Initializing Nexus Manager...")
-    # Create Nexus manager and call methods on Nexus manager
+    # Variable declarations -----------------------------------------
     nexus_overlord = NexusManager()
-    print("Pipeline capacity: 1000 streams/second")
-    print("")
+
+    input_stage: InputStage = InputStage()
+    transform_stage: TransformStage = TransformStage()
+    output_stage: OutputStage = OutputStage()
+    json_pipeline: JSONAdapterPipeLine = JSONAdapterPipeLine("JSON_01")
+    csv_pipeline: CSVAdapterPipeLine = CSVAdapterPipeLine("CSV_01")
+    stream_pipeline: StreamAdapterPipeLine = StreamAdapterPipeLine("STREAM_01")
+    stages_list = [input_stage, transform_stage, output_stage]
+    adapters_list = [json_pipeline, csv_pipeline, stream_pipeline]
+
+    json_data: dict = {"sensor": "temp", "value": 23.5, "unit": "C"}
+    csv_data: str = "user,action,timestamp"
+    stream_data: str = "Real-time sensor stream"
+
+    # Header --------------------------------------------------------
+    print("=== CODE NEXUS - ENTERPRISE PIPELINE SYSTEM ===\n")
+
+    # Initialise NexusManager ---------------------------------------
+    if nexus_overlord:
+        print("Initializing Nexus Manager...")
+    print("Pipeline capacity: 1000 streams/second\n")
+
+    # Creating a pipeline -------------------------------------------
     print("Creating Data Processing Pipeline...")
     print("Stage 1: Input validation and parsing")
-    # set up input -> on Nexus manager have set_up_input method
-    # create input stage and add to list of pipelines
+    json_pipeline.add_stage(input_stage)
     print("Stage 2: Data transformation and enrichment")
-    # Same but for Transform stage
-    print("Stage 3: Output formatting and delivery")
-    # Same but for Output stage
-    print("")
-    print("=== Multi-Format Data Processing ===")
-    print("")
+    json_pipeline.add_stage(transform_stage)
+    print("Stage 3: Output formatting and delivery\n")
+    json_pipeline.add_stage(output_stage)
+    nexus_overlord.add_pipeline(json_pipeline)
+
+    # Creating next two pipelines -----------------------------------
+    for pipeline in [csv_pipeline, stream_pipeline]:
+        pipeline.add_stage(input_stage)
+        pipeline.add_stage(transform_stage)
+        pipeline.add_stage(output_stage)
+        nexus_overlord.add_pipeline(pipeline)
+
+    # Demonstrate pipeline works with any data ----------------------
+    print("=== Multi-Format Data Processing ===\n")
+
     print("Processing JSON data through pipeline...")
     print('Input: {"sensor": "temp", "value": 23.5, "unit": "C"}')
     print("Transform: Enriched with metadata and validation")
