a39f3a8d2da56303f6bddfe70867dbbd26cd1c20
Leandra Lourens <llourens.codam@gmail.com>
2026-04-24 15:26:44 +0200

    🐛 Module 07 required a single battle function.

--- Module_07/tournament.py
+++ Module_07/tournament.py
@@ -1,64 +1,64 @@
 #!/usr/bin/env python3
 
-from typing import List
-from ex0 import creature_classes
+from typing import List, Tuple
 from ex0.creature_classes import Creature
+from ex0.creature_factory_classes import CreatureFactory
 from ex0 import FlameFactory, AquaFactory
 from ex1 import HealingCreatureFactory, TransformCreatureFactory
 from ex2 import battle_strategy_classes
 from ex2.battle_strategy_classes import BattleStrategy
 
 
+def battle(opponents: List[Tuple[CreatureFactory, BattleStrategy]]) -> None:
+    opp_1: int
+
+    for opp_1 in range(len(opponents)):
+        for opp_2 in range(opp_1 + 1, len(opponents)):
+            factory_1, strategy_1 = opponents[opp_1]
+            factory_2, strategy_2 = opponents[opp_2]
+            opponent_1: Creature = factory_1.create_base()
+            opponent_2: Creature = factory_2.create_base()
+            print("* Battle *")
+            print(f"{opponent_1.describe()}")
+            print(" vs.")
+            print(f"{opponent_2.describe()}")
+            print(" now fight!")
+            try:
+                print(f"{strategy_1.act(opponent_1)}")
+                print(f"{strategy_2.act(opponent_2)}\n")
+            except ValueError as msg:
+                raise ValueError(msg)
+
+
 def tournament() -> None:
     # Variables ***************************************************************
-    opponents: List[creature_classes.Creature] = []
-    flame_factory: FlameFactory = FlameFactory()
-    aqua_factory: AquaFactory = AquaFactory()
-    healing_factory: HealingCreatureFactory = HealingCreatureFactory()
-    transform_factory: TransformCreatureFactory = TransformCreatureFactory()
-    opponent_flameling: Creature = flame_factory.create_base()
-    opponent_aquabub: Creature = aqua_factory.create_base()
-    opponent_sproutling: Creature = healing_factory.create_base()
-    opponent_shiftling: Creature = transform_factory.create_base()
-    flameling_strategy: BattleStrategy = (
-        battle_strategy_classes.NormalStrategy())
-    sproutling_strategy: BattleStrategy = (
-        battle_strategy_classes.DefensiveStrategy())
-    aquabub_strategy: BattleStrategy = battle_strategy_classes.NormalStrategy()
-    shiftling_strategy: BattleStrategy = (
-        battle_strategy_classes.AggressiveStrategy())
+    opponents: List[Tuple[CreatureFactory, BattleStrategy]] = []
 
     # Tournament 0 -> Basic tournament ****************************************
 
     print("Tournament 0 (basic)")
     print(" [ (Flameling+Normal), (Healing+Defensive) ]")
     print("*** Tournament **")
-    opponents = [opponent_flameling, opponent_sproutling]
+    opponents = [(FlameFactory(), battle_strategy_classes.NormalStrategy()),
+                 (HealingCreatureFactory(),
+                  battle_strategy_classes.DefensiveStrategy())]
     print(f"{len(opponents)} opponents involved\n")
-
-    print("* Battle *")
-    print(f"{opponent_flameling.describe()}")
-    print(" vs.")
-    print(f"{opponent_sproutling.describe()}")
-    print(" now fight!")
-    print(f"{flameling_strategy.act(opponent_flameling)}")
-    print(f"{sproutling_strategy.act(opponent_sproutling)}\n")
+    try:
+        battle(opponents)
+    except ValueError as msg:
+        print(f"{msg}\n")
 
     # Tournament 1 -> Error test **********************************************
     print("Tournament 1 (error)")
     print(" [ (Flameling+Aggressive), (Healing+Defensive) ]")
     print("*** Tournament ***")
-    flameling_strategy = battle_strategy_classes.AggressiveStrategy()
+    opponents = [(FlameFactory(),
+                  battle_strategy_classes.AggressiveStrategy()),
+                 (HealingCreatureFactory(),
+                  battle_strategy_classes.DefensiveStrategy())]
     print(f"{len(opponents)} opponents involved\n")
-
-    print("* Battle *")
-    print(f"{opponent_flameling.describe()}")
-    print(" vs.")
-    print(f"{opponent_sproutling.describe()}")
-    print(" now fight!")
     try:
-        print(f"{flameling_strategy.act(opponent_flameling)}")
-        print(f"{sproutling_strategy.act(opponent_sproutling)}\n")
+        battle(opponents)
     except ValueError as msg:
         print(f"{msg}\n")
 
@@ -66,39 +66,14 @@ def tournament() -> None:
     print("Tournament 2 (multiple)")
     print(" [ (Aquabub+Normal), (Healing+Defensive), (Transform+Aggressive) ]")
     print("*** Tournament ***")
-    opponents = [opponent_aquabub, opponent_sproutling, opponent_shiftling]
+    opponents = [(AquaFactory(), battle_strategy_classes.NormalStrategy()),
+                 (HealingCreatureFactory(),
+                  battle_strategy_classes.DefensiveStrategy()),
+                 (TransformCreatureFactory(),
+                  battle_strategy_classes.AggressiveStrategy())]
     print(f"{len(opponents)} opponents involved\n")
-
-    print("* Battle *")
-    print(f"{opponent_aquabub.describe()}")
-    print(" vs.")
-    print(f"{opponent_sproutling.describe()}")
-    print(" now fight!")
     try:
-        print(f"{aquabub_strategy.act(opponent_aquabub)}")
-        print(f"{sproutling_strategy.act(opponent_sproutling)}\n")
-    except ValueError as msg:
-        print(f"{msg}\n")
-
-    print("* Battle *")
-    print(f"{opponent_aquabub.describe()}")
-    print(" vs.")
-    print(f"{opponent_shiftling.describe()}")
-    print(" now fight!")
-    try:
-        print(f"{aquabub_strategy.act(opponent_aquabub)}")
-        print(f"{shiftling_strategy.act(opponent_shiftling)}\n")
-    except ValueError as msg:
-        print(f"{msg}\n")
-
-    print("* Battle *")
-    print(f"{opponent_sproutling.describe()}")
-    print(" vs.")
-    print(f"{opponent_shiftling.describe()}")
-    print(" now fight!")
-    try:
-        print(f"{sproutling_strategy.act(opponent_sproutling)}")
-        print(f"{shiftling_strategy.act(opponent_shiftling)}")
+        battle(opponents)
     except ValueError as msg:
         print(f"{msg}\n")
 
