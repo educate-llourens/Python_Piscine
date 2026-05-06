17d10e56f7789a1d20130b2f5b03a51127bcf6a8
Leandra Lourens <llourens.codam@gmail.com>
2026-05-06 14:20:32 +0200

    🚧 Completed ex3 with flake8 and mypy checks

--- Module_10/ex3/functools_artifacts.py
+++ Module_10/ex3/functools_artifacts.py
@@ -1,8 +1,8 @@
 #!/usr/bin/env python3
 
-from functools import reduce, partial
+from functools import reduce, partial, lru_cache, singledispatch
 from operator import add, mul
-from typing import List
+from typing import List, Any
 from collections.abc import Callable
 
 
@@ -23,17 +23,52 @@ def base_enchantment(power: int, element: str, target: str) -> str:
 
 
 def partial_enchanter(base_enchantment: Callable) -> dict[str, Callable]:
-    enchantment: Callable
+    water_enchantment: Callable
+    ground_enchantment: Callable
+    metal_enchantment: Callable
 
-    enchantment = partial(base_enchantment, 50, "Fire")
-    return {"fire_enchanter": enchantment}
+    water_enchantment = partial(base_enchantment, 50, "water")
+    ground_enchantment = partial(base_enchantment, 50, "ground")
+    metal_enchantment = partial(base_enchantment, 50, "metal")
+    return {"water": water_enchantment,
+            "ground": ground_enchantment,
+            "metal": metal_enchantment}
+
+
+@lru_cache
+def memoized_fibonacci(n: int) -> int:
+    if n < 2:
+        return n
+    return (memoized_fibonacci(n - 1) + memoized_fibonacci(n - 2))
+
+
+def spell_dispatcher() -> Callable[[Any], str]:
+    @singledispatch
+    def dispatch_spell(value: Any) -> str:
+        return "Unknown spell type"
+
+    @dispatch_spell.register(int)
+    def dispatch_damage_spell(value: int) -> str:
+        return f"Damage spell: {value}"
+
+    @dispatch_spell.register(str)
+    def dispatch_enchantment(enchantment: str) -> str:
+        return f"Enchantment: {enchantment}"
+
+    @dispatch_spell.register(list)
+    def dispatch_multicast(spells_list: List) -> str:
+        return f"Multi-cast: {len(spells_list)} spells"
+    return dispatch_spell
 
 
 def main() -> None:
     # Variables ***************************************************************
     reducer_list: List[int]
+    enchantments: dict
+    dispatch_spells: Callable[[int], str]
 
     # Testing Spell Reducer ***************************************************
+    print("\nTesting spell reducer...")
     reducer_list = [10, 10, 50, 30]
     print(f"Sum: {spell_reducer(reducer_list, "add")}")
     print(f"Product: {spell_reducer(reducer_list, "multiply")}")
@@ -41,7 +76,32 @@ def main() -> None:
     print(f"Min: {spell_reducer(reducer_list, "min")}\n")
 
     # Testing Partial Enchanter ***********************************************
-    
+    print("Testing partial enchanter...")
+    enchantments = partial_enchanter(base_enchantment)
+    print(enchantments['water']("Druid"))
+    print(enchantments['ground']("Violet"))
+    print(enchantments['metal']("Knight"))
+    print("")
+
+    # Testing Memoized Fibonacci **********************************************
+    print("Testing memoized fibonacci...")
+    print(f"Fib(0): {memoized_fibonacci(0)}", end=", ")
+    print(f"Number of cache hits: {memoized_fibonacci.cache_info().hits}")
+    print(f"Fib(1): {memoized_fibonacci(1)}", end=", ")
+    print(f"Number of cache hits: {memoized_fibonacci.cache_info().hits}")
+    print(f"Fib(10): {memoized_fibonacci(10)}", end=", ")
+    print(f"Number of cache hits: {memoized_fibonacci.cache_info().hits}")
+    print(f"Fib(15): {memoized_fibonacci(15)}", end=", ")
+    print(f"Number of cache hits: {memoized_fibonacci.cache_info().hits}")
+    print("")
+
+    # Testing Spell Dispatcher ************************************************
+    print("Testing spell dispatcher...")
+    dispatch_spells = spell_dispatcher()
+    print(dispatch_spells(42))
+    print(dispatch_spells("fireball"))
+    print(dispatch_spells([42, 36, 72]))
+    print(dispatch_spells({'spell': "Try this"}))
 
 
 if __name__ == "__main__":
