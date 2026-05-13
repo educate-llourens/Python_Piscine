e07beae48a853e3e12594c69673e500f6d4b624c
Leandra Lourens <llourens.codam@gmail.com>
2026-05-13 13:00:09 +0200

    🚧 Completed Module 10 including mypy and flake8 checks

--- Module_10/ex4/decorator_mastery.py
+++ Module_10/ex4/decorator_mastery.py
@@ -6,25 +6,16 @@ from time import perf_counter
 from collections.abc import Callable
 
 
-class MageGuild:
-    @staticmethod
-    def validate_mage_name(name: str) -> bool:
-        return True
-
-    def cast_spell(self, spell_name: str, power: int) -> str:
-        return ""
-
-
-def spell_timer(func: Callable) -> Callable:
-    @wraps(func)
+def spell_timer(function: Callable) -> Callable:
+    @wraps(function)
     def wrapped_function(*pos_args, **key_word_args) -> Callable:
         start_timer: float
         end_timer: float
         result: Any
 
-        print(f"Casting {func.__name__}...")
+        print(f"Casting {function.__name__}...")
         start_timer = perf_counter()
-        result = func(*pos_args, **key_word_args)
+        result = function(*pos_args, **key_word_args)
         end_timer = perf_counter()
         # Change to 7 decimal places to see result
         print(f"Spell completed in {end_timer - start_timer:.3f} seconds")
@@ -38,29 +29,67 @@ def fireball() -> str:
 
 
 def power_validator(min_power: int) -> Callable:
-    return (spell_timer)
+    def validate_power(function: Callable) -> Callable:
+        @wraps(function)
+        def wrapped_function(*pos_args, **key_word_args) -> Any:
+            if pos_args and pos_args[2] >= min_power:
+                return function(*pos_args, **key_word_args)
+            return "Insufficient power for this spell"
+        return wrapped_function
+    return validate_power
 
 
 def retry_spell(max_attempts: int) -> Callable:
     def retry(function: Callable) -> Callable:
-        retry_counter: int = 0
+        @wraps(function)
+        def wrapped_function(*pos_args, **key_word_args) -> Any:
+            retry_counter: int = 0
 
-        @wraps
-        def wrapped_function(*pos_args, **key_word_args) -> Callable:
-            result: Any
-            nonlocal retry_counter
-
-            try:
-                result = function(*pos_args, **key_word_args)
-            except Exception:
-                retry_counter += 1
-            return result
+            while retry_counter < max_attempts:
+                try:
+                    return function(*pos_args, **key_word_args)
+                except Exception:
+                    retry_counter += 1
+                    if retry_counter == max_attempts:
+                        return ("Spell casting failed after "
+                                f"{max_attempts} attempts")
+                    else:
+                        print("Spell failed, retrying... (attempt "
+                              f"{retry_counter}/{max_attempts})")
         return wrapped_function
     return retry
 
 
+class MageGuild:
+    @staticmethod
+    def validate_mage_name(name: str) -> bool:
+        if len(name) >= 3:
+            for item in name:
+                if not item.isalpha() and item != " ":
+                    return False
+            return True
+        return False
+
+    @power_validator(10)
+    def cast_spell(self, spell_name: str, power: int) -> str:
+        return f"Successfully cast {spell_name} with {power} power"
+
+
+@retry_spell(3)
+def victim_scream(hit_successful: bool) -> str:
+    if not hit_successful:
+        raise ValueError("Hit missed")
+    return "Waaaaaaagh spelled !"
+
+
+@power_validator(10)
+def enough_power(spell_name: str, damage_type: str, power: int) -> str:
+    return f"{spell_name} dealt {damage_type} damage for {power} points"
+
+
 def main() -> None:
     # Variables ***************************************************************
+    guild = MageGuild()
 
     # Testing Spell Timer *****************************************************
     print("Testing spell timer...")
@@ -68,6 +97,17 @@ def main() -> None:
     print("")
 
     # Testing Retrying spell **************************************************
+    print("Testing retrying spell...")
+    print(victim_scream(False))
+    print(victim_scream(True))
+    print("")
+
+    # Testing MageGuild class *************************************************
+    print("Testing MageGuild...")
+    print(guild.validate_mage_name("This will pass"))
+    print(guild.validate_mage_name("This_will_fail"))
+    print(guild.cast_spell("Lightning", 15))
+    print(guild.cast_spell("Fire", 5))
 
 
 if __name__ == "__main__":
